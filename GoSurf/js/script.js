060db80440bdf350a7b6bbb960d2fa07a68a6174
Loloops <61906221+Loloops@users.noreply.github.com>
2021-08-21 11:51:12 +0400

    ending second block

--- GoSurf/js/script.js
+++ GoSurf/js/script.js
@@ -1,28 +1,44 @@
 $(function(){
 
+  /* HEADER */
   $('.header__slider').slick({
     infinite: true,
     fade: true,
     
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/header/ArrowLeft.svg" alt=""></img>',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/header/ArrowRight.svg" alt=""></img>',
-    asNavFor: '.slider-dots'
+    asNavFor: '.slider-dotshead'
   });
 
-  $('.slider-dots').slick({
+  $('.slider-dotshead').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     infinite: false,
    
     asNavFor: '.header__slider'
   });
+/* /HEADER */
 
+/* SURF */
   $('.serf-slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/header/ArrowLeft.svg" alt=""></img>',
-    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/header/ArrowRight.svg" alt=""></img>'
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/header/ArrowRight.svg" alt=""></img>',
+    asNavFor: '.slider-map'
   });
+
+  $('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 8,
+    arrows: false,
+    asNavFor: '.serf-slider',
+    focusOnSelect: true,
+    infinite: false
+  });
+/* /SURF */
+
+
 
 });
 
